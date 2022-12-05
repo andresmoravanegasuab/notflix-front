293b69d9f55d1c02fa1c06d1439d998d260f2d4c
Andres Mora Vanegas <andresmoraprofesor@gmail.com>
2022-12-05 09:02:28 -0500

    [init] First commit of project

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import logo from '../../assets/img/netflix.webp';
+
+export const Header = ()=>{
+
+    return (
+        <div>
+            <img src={logo} alt="logo" />
+            <h1>Bienvenido</h1>
+        </div>
+    )
+}
